7c306d1e1b8124f03544f10e25e8eca4d4702bab
html-deng <1227294378@qq.com>
2026-05-31 20:15:51 +0800

    css
    更新

---
+++ js/微星编程.js
@@ -0,0 +1,12 @@
+var color="write"
+function white_and_black():
+    function date(): 
+        var body = document.getElementById("back");
+        if(color=="white"){
+            body.style.color="white";
+            color="black";
+        }
+        else{
+            body.style.color="black";
+            color="white";
+        }
